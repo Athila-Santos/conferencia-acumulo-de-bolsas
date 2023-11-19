fed488f11d4b1be2756a738a6b725cd164a09927
Athila-Santos <athila.silva@ufpe.br>
2023-11-18 23:35:47 -0300

    Teste do código em .exe
    consegui rodar o código em um arquivo executável com sucesso, mas para funcionalidade plena desse arquivo ele precisa levar em consideração  a alteração de outros parâmetros além da base de favorecidos, como:
    
    - data;
    - período de interesse a ser analisado.
    
    desse modo, resta testar a adição de um relatório em xlxs contendo a pesquisa geral para outras possíveis análises do setor financeiro, além de contar com o fator data. Para o segundo problema, penso que uma msgbox para input seja o suficiente, assim o usuário interage com o executável através de uma interface simples e torna a seleção do período de análise requerido sem erros.

--- Api_favorecido_final_portal.py
+++ Api_favorecido_final_portal.py
@@ -1,54 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # *Bibliotecas*
-
-# In[ ]:
-
-
-pip install ratelimiter
-
-
-# In[ ]:
-
-
 import requests
 import pandas as pd
 from ratelimiter import RateLimiter
-# import os
+
 # os.getcwd()
 # os.chdir('*****')
 
 
-# # Importando relação de favorecidos
-# 
-# ---
+# Importando relação de favorecidos
 
-# In[ ]:
-
-
-cpfs = pd.read_csv('codpessoa.csv',dtype={'favorecido': str})
+cpfs = pd.read_csv('codpessoa.csv', dtype = {'favorecido': str})
 
 favorecidos = cpfs.values.tolist()
-len(favorecidos)
 
-
-# 
-# # Conexão *API* do Portal da Transparência
-
-# In[ ]:
-
+# Conexão *API* do Portal da Transparência
 
 #url recurso recebido
+
 url = "https://api.portaldatransparencia.gov.br/api-de-dados/despesas/recursos-recebidos"
 
 #params
+
 codigoFavorecido = favorecidos
 mesAnoFim = '12/2023'
 mesAnoInicio = '09/2023'
 pagina = 1
 
 # Monta o dicionário com as informações capturadas
+
 params = {"codigoFavorecido": codigoFavorecido,
           "mesAnoFim": mesAnoFim,
           "mesAnoInicio": mesAnoInicio,
@@ -56,20 +34,18 @@ params = {"codigoFavorecido": codigoFavorecido,
           }
 
 #chave da API
+
 headers = {"chave-api-dados":"1f580edc90fe43ea4a082e6ea3590ca6"}
 
 
 # *Onde a mágica acontece*
-# ---
-
-# In[ ]:
 
 
 soma_despesas = pd.DataFrame()
 codigoFavorecido = favorecidos
 
 # limitador de taxa para permitir no máximo x requisições por minuto
-limite = RateLimiter(max_calls=395, period=60)
+limite = RateLimiter(max_calls = 350, period = 60)
 
 for i in range(0,len(codigoFavorecido)):
     # limitador de taxa em nosso loop
@@ -79,15 +55,12 @@ for i in range(0,len(codigoFavorecido)):
                   "mesAnoInicio": mesAnoInicio,
                   "pagina": pagina,
                   }
-        resultado = requests.get(url, params=params, headers=headers)
+        resultado = requests.get(url, params = params, headers = headers)
         despesas = pd.DataFrame(resultado.json())
-        soma_despesas = pd.concat([soma_despesas,despesas])
+        soma_despesas = pd.concat([soma_despesas, despesas])
 
 
-# # *Tratamento dos dados obtidos*
-
-# In[ ]:
-
+# *Tratamento dos dados obtidos*
 
 soma_despesas_resumido = soma_despesas[['anoMes',
                                         'nomePessoa',
@@ -106,10 +79,7 @@ soma_despesas_resumido.rename(columns={'anoMes':'Ano/Mes',
                                         }, inplace=True)
 
 
-# # Filtro de acúmulos
-
-# In[14]:
-
+# Filtro de acúmulos
 
 fora_do_escopo = soma_despesas_resumido.query(" `Unidade Gestora` != ['PRO-REITORIA DE ASSUNTOS ESTUDANTIS - PROAES','PRO-REITORIA DE EXTENSAO'] and `Ano/Mes` == [202310, 202311] ")
 
@@ -118,18 +88,13 @@ fora_do_escopo = soma_despesas_resumido.query(" `Unidade Gestora` != ['PRO-REITO
 nomes = fora_do_escopo['Nome'].unique()
 possivel_acumulo = soma_despesas_resumido[soma_despesas_resumido['Nome'].isin(nomes)]
 
-
-# # Export to excel
-
-# In[19]:
-
+# Export to csv
 
 possivel_acumulo.to_csv('possivel_acumulo.csv',encoding = 'utf-8-sig', index = False)
 
 
-# hipótese de inserir todos os dados em um xlxs
+# hipótese de inserir todos os dados em um xlsx
 
 # with pd.ExcelWriter('panorama.xlsx') as writer:  
 #     possivel_acumulo.to_excel(writer, sheet_name='Acúmulo')
 #     soma_despesas_resumido.to_excel(writer, sheet_name='Geral')
-
